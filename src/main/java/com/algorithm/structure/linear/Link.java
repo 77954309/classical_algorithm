71db8031d0a500c64aa8a5e0b97f2d5679781a7d
limeng <limeng@hushijie.com.cn>
2018-12-05 20:42:03 +0800

    初始化

---
+++ src/main/java/com/algorithm/structure/linear/Link.java
@@ -0,0 +1,32 @@
+package com.algorithm.structure.linear;
+
+/**
+ * 链接点 数据下一个指针
+ *
+ * @author limeng
+ * @create 2018-12-05 下午6:42
+ **/
+public class Link {
+    private long data;
+    private Link next;
+
+    public Link(long data) {
+        this.data = data;
+    }
+
+    public long getData() {
+        return data;
+    }
+
+    public void setData(long data) {
+        this.data = data;
+    }
+
+    public Link getNext() {
+        return next;
+    }
+
+    public void setNext(Link next) {
+        this.next = next;
+    }
+}
---
+++ src/main/java/com/algorithm/structure/linear/StaticList.java
@@ -0,0 +1,11 @@
+package com.algorithm.structure.linear;
+
+/**
+ * 静态链表
+ *
+ * @author limeng
+ * @create 2018-12-05 下午6:50
+ **/
+public class StaticList {
+    private Link[] links=new Link[50];
+}
