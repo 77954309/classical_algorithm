798645d0b9b9fe6749105a7feaacee9e0711cb11
limeng <limeng@hushijie.com.cn>
2018-12-06 19:33:24 +0800

    初始化

--- src/main/java/com/algorithm/structure/linear/CycleLinkList.java
+++ src/main/java/com/algorithm/structure/linear/CycleLinkList.java
@@ -7,6 +7,7 @@ import org.junit.Test;
  * 尾部指向头部
  * 跟单链表区别判断链表结尾
  *
+ * 约瑟夫循环链表 josephRemove
  * @author limeng
  * @create 2018-12-06 下午3:31
  **/
@@ -73,6 +74,44 @@ public class CycleLinkList {
 
     }
 
+    protected int linkLength(Link link){
+        int length=0;
+        Link tmp=link;
+        while (tmp.getNext() != first){
+            tmp=tmp.getNext();
+            ++length;
+        }
+        return length;
+    }
+
+    /**
+     * 约瑟夫循环链表
+     *
+     * 41个人围成一个圈，每到3杀人
+     */
+    protected void josephRemove(){
+        Link tmp = first;
+        int index=0;
+        while (true){
+            tmp = tmp.getNext();
+            if(tmp!=first){
+                ++index;
+            }
+            if(tmp.getNext() == first && first.getNext().getNext() == tmp){
+                System.out.println("就剩两个人了-----------------------------");
+                this.displayAll();
+                break;
+            }
+
+            if(index == 3 && tmp!=first){
+                index = 0;
+                System.out.println(tmp.getData());
+                this.remove(tmp.getData());
+            }
+        }
+    }
+
+
     /**
      * 显示
      */
@@ -86,7 +125,7 @@ public class CycleLinkList {
 
     @Test
     public void init(){
-        this.initList();
+        /*this.initList();
         this.insert(1);
         this.insert(2);
         this.insert(3);
@@ -94,7 +133,15 @@ public class CycleLinkList {
         this.insert(11,2);
 
 
-        this.remove(11);
+        this.remove(11);*/
+
+       /* //约瑟夫循环链表
+        this.initList();
+        for (int i = 0; i < 41; i++) {
+            this.insert(i+1);
+        }
+        this.josephRemove();*/
+
         this.displayAll();
     }
 }
