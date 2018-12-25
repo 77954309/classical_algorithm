1e7fc845d83aaca5fb937ab6a56a84e73c7b7372
limeng <limeng@hushijie.com.cn>
2018-12-25 19:37:45 +0800

    初始化

---
+++ src/main/java/com/algorithm/structure/chart/AdjacencyTable.java
@@ -0,0 +1,32 @@
+package com.algorithm.structure.chart;
+
+import org.junit.Test;
+
+/**
+ * 邻接表
+ *
+ * @author limeng
+ * @create 2018-12-25 下午4:44
+ **/
+public class AdjacencyTable {
+
+    @Test
+    public void undirectedInit(){
+
+    }
+
+    @Test
+    public void directedInit(){
+
+    }
+
+}
+
+class UndirectedTable{
+    private Table first;
+}
+
+class DirectedTable{
+    private Table first;
+
+}
