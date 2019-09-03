996d6249487c980f9a1ab151f7564eaee62b0301
李孟 <limeng@knowlegene.com>
2019-09-03 11:57:17 +0800

    排序练习

--- src/main/java/com/algorithm/sort/SortTest.java
+++ src/main/java/com/algorithm/sort/SortTest.java
@@ -1,14 +1,70 @@
 package com.algorithm.sort;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 排序测试
  * @Author: limeng
  * @Date: 2019/9/2 22:12
  */
 public class SortTest  {
+    /**
+     * 冒泡
+     */
+   @Test
    public void bubble(){
-
+      int[] a=new int[]{0,12,3,4,1,3,4,5,6};
+      int size = a.length;
+      for (int j = 0; j < size; j++) {
+         for(int k=j;k < size;k++){
+            if(a[j] < a[k]){
+                  int tmp = a[j];
+                  a[j] = a[k];
+                  a[k] = tmp;
+            }
+         }
+      }
+       Assert.assertNotNull(a);
+   }
+   @Test
+   public void insert(){
+       int[] a=new int[]{0,12,3,4,1,3,4,5,6};
+       int size = a.length;
+       for (int i = 1; i < size; i++) {
+           int j = i -1;
+           int value = a[i];
+           for (;j > 0;--j){
+               if(a[j] > value){
+                   a[j+1] = a[j];
+                   //移位
+               }else{
+                   break;
+               }
+           }
+           //插值
+           a[j+1] = value;
+       }
+       Assert.assertNotNull(a);
    }
 
+   @Test
+   public void select(){
+       int[] a=new int[]{0,12,3,4,1,3,4,5,6};
+       int size =a.length;
+       for (int i = 1; i < size; i++) {
+           int min = i;
+           for(int j=i+1;j<size;j++){
+               if(a[j] < a[min]){
+                   min = j;
+               }
+           }
+
+           int tmp =a[min];
+           a[min] = a[i];
+           a[i] = tmp;
+       }
+       Assert.assertNotNull(a);
+   }
 }
