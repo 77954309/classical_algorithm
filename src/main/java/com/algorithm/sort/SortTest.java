ece0bd2a1727af81c0d7f8372e1700687cab3142
limeng <77954309@qq.com>
2019-09-08 12:43:42 +0800

    排序

--- src/main/java/com/algorithm/sort/SortTest.java
+++ src/main/java/com/algorithm/sort/SortTest.java
@@ -171,9 +171,43 @@ public class SortTest  {
 
     }
 
+   @Test
    public void quickSort(){
-
+       int[] a=new int[]{10,45,17,89,21};
+       quickSortInternally(a, 0, a.length-1);
    }
+
+    // 快速排序递归函数，p,r为下标
+    private  void quickSortInternally(int[] a, int p, int r) {
+       if(p >= r) return;
+
+        int q=partition(a,p,r);
+        quickSortInternally(a,p,q-1);
+        quickSortInternally(a,q+1,r);
+    }
+
+    private int partition(int[] a,int p,int r){
+       int pivot = a[r];
+       int i = p;
+       for(int j=p;j<r;++j){
+           if(a[j] < pivot){
+               if(i == j){
+                   ++i;
+               }else{
+                    int tmp = a[i];
+                    a[i++] = a[j];
+                    a[j] =tmp;
+               }
+           }
+       }
+
+       int tmp = a[i];
+       a[i] = a[r];
+       a[r] = tmp;
+       return i;
+    }
+
+
 
 
 
