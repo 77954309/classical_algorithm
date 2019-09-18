f722db4f823ce3884de49bf9cee399361dcfafad
limeng <77954309@qq.com>
2019-09-19 00:31:55 +0800

    排序

--- src/main/java/com/algorithm/find/BinaryFind2.java
+++ src/main/java/com/algorithm/find/BinaryFind2.java
@@ -1,5 +1,8 @@
 package com.algorithm.find;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  *
@@ -11,7 +14,35 @@ public class BinaryFind2 {
     /**
      * 平方根，精确小数点后6位
      */
+    @Test
     public void testBinarySqrt(){
+        sqrt(16);
+    }
+
+
+    public void sqrt(int x){
+        double lo=1;
+        double hi=x;
+        if(x<1){
+            lo=x;
+            hi=1;
+        }
+
+        double mid=lo+(hi-lo)/2;
+        double precision=0.000001;
+
+        while (Math.abs(mid*mid-x)>precision){
+            if(mid*mid < x){
+                lo = mid;
+            }else{
+                hi = mid;
+            }
+
+            mid =(hi+lo)/2;
+
+        }
+
+        Assert.assertNotNull(mid);
 
     }
 
