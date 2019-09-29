1b748299faa9d8a4eab2a7704198f14d2aa028bc
李孟 <limeng@knowlegene.com>
2019-09-29 19:53:17 +0800

    查找练习

---
+++ src/main/java/com/algorithm/structure/chart/JumpTable.java
@@ -0,0 +1,11 @@
+package com.algorithm.structure.chart;
+
+/**
+ * https://blog.csdn.net/gloomysnow/article/details/51510203
+ * 跳表
+ * @Author: limeng
+ * @Date: 2019/9/29 18:49
+ */
+public class JumpTable {
+
+}
