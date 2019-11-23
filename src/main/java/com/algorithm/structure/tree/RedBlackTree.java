5577b5d5fd990c4f0e4bbbc7e877c7dbfdcc0a40
李孟 <limeng@knowlegene.com>
2019-11-23 10:40:55 +0800

    二叉树

--- src/main/java/com/algorithm/structure/tree/RedBlackTree.java
+++ src/main/java/com/algorithm/structure/tree/RedBlackTree.java
@@ -2,6 +2,10 @@ package com.algorithm.structure.tree;
 
 /**
  *
+ * 1.根节点是黑色
+ * 2.每个叶子节点都是黑色的空节点（NIL）,也就是说，叶子节点不存储数据
+ * 3.任何相邻的节点都不能同时为红色，也就是说，红色节点是被黑色节点隔开的；
+ * 4.每个节点，从该节点到达其可达叶子节点的所有路径，都包含相同数目的黑色节点；
  * @Classname RedBlackTree
  * @Description TODO
  * @Date 2019/11/19 11:27
