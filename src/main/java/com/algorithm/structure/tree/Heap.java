95e227c1f23cb8af93339f1b1624562684779005
李孟 <limeng@knowlegene.com>
2019-12-08 17:28:42 +0800

    堆

---
+++ src/main/java/com/algorithm/structure/tree/Heap.java
@@ -0,0 +1,26 @@
+package com.algorithm.structure.tree;
+
+/**
+ * 堆
+ *  堆是一个完全二叉树。
+ *  堆中每一个节点的值都必须大于等于（小于等于）其子树中每个节点的值。
+ *
+ *
+ *  大顶堆
+ * @Classname Heap
+ * @Description TODO
+ * @Date 2019/12/8 15:58
+ * @Created by limeng
+ */
+public class Heap {
+    //从下标1开始存储数据
+    private int[] a;
+    //堆可以存储的最大数据个数
+    private int n;
+    //堆中已经存储的数据个数
+    private int count;
+
+
+
+
+}
