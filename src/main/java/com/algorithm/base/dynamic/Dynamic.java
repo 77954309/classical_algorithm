a505d9b4df38a8824ee6d240b1cd02e5ec3c6a2d
limeng <77954309@qq.com>
2020-02-28 08:26:30 +0800

    动态规划

--- src/main/java/com/algorithm/base/dynamic/Dynamic.java
+++ src/main/java/com/algorithm/base/dynamic/Dynamic.java
@@ -76,30 +76,33 @@ public class Dynamic {
 
 
     /**
+     * min_dist(i,j) = w[i][j] + min(min_dist(i,j-1),min_dist(i-1,j))
      * 状态转移表
      * @param matrix
      * @param n
      * @return
      */
     public int mindistDP(int[][] matrix,int n){
-       int[][] states =new int[n][n];
-       int sum = 0;
+      int[][] states =new int[n][n];
+      int sum=0;
 
-        for (int j = 0; j < n; j++) {
-            sum += matrix[0][j];
-            states[0][j] = sum;
-        }
-
-        sum = 0;
+        //行
         for (int i = 0; i < n; i++) {
-            sum +=matrix[i][0];
+            sum += matrix[i][0];
             states[i][0] = sum;
         }
 
+        sum = 0;
+
+        //列
+        for (int j = 0; j < n; j++) {
+            sum +=matrix[0][j];
+            states[0][j] = sum;
+        }
 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < n; j++) {
-                states[i][j] = matrix[i][j] + Math.min(states[i][j-1],states[i-1][j]);
+                states[i][j] += Math.min(states[i-1][j],states[i][j-1]);
             }
         }
 
@@ -107,16 +110,44 @@ public class Dynamic {
     }
 
 
+    int[][] matrix = {{1,3,5,9}, {2,1,3,4},{5,2,6,7},{6,8,4,3}};
+    int n2 =4;
+    int[][] mem2 =new int[4][4];
+    public int minDist(int i,int j){
+        if(i == 0 && j == 0) return matrix[0][0];
+        if(mem2[i][j] > 0 )return mem2[i][j];
+
+
+        int minLeft = Integer.MAX_VALUE;
+        if(j-1 >= 0){
+            minLeft = minDist(i,j-1);
+        }
+
+        int minUp = Integer.MAX_VALUE;
+        if(i -1 >= 0){
+            minUp = minDist(i-1,j);
+        }
+
+        int currMinDist = matrix[i][j] + Math.min(minLeft,minUp);
+        mem2[i][j] = currMinDist;
+        return currMinDist;
+    }
+
+
     @Test
     public void  init(){
 
         int[][] w={{1,3,5},{2,1,3},{5,2,6}};
-        minDistBT(0,0,0,w,w.length);
+ //       minDistBT(0,0,0,w,w.length);
 //
 //
-        Assert.assertNotNull(minDist);
+//        Assert.assertNotNull(minDist);
 
      //   int dp = mindistDP(w, w.length);
      //   Assert.assertNotNull(dp);
+
+
+        minDist(3,3);
+
     }
 }
