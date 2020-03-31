2dee120a3711600501925b3dfdc51c0f97cb267f
李孟 <limeng@knowlegene.com>
2020-03-31 20:53:50 +0800

    最短路径

---
+++ src/main/java/com/algorithm/base/bitmap/BitMap.java
@@ -0,0 +1,17 @@
+package com.algorithm.base.bitmap;
+
+/**
+ * Java中char类型占16bit,也即是2个字节
+ * @Classname BitMap
+ * @Description TODO
+ * @Date 2020/3/27 9:47
+ * @Created by limeng
+ */
+public class BitMap {
+    private char[] bytes;
+    private int nbits;
+
+
+
+
+}
