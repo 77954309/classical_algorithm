4c841b1fb5d0742dd5a67c75dbb9587b1b00206c
李孟 <limeng@knowlegene.com>
2020-04-04 05:21:45 +0800

    位图

--- src/main/java/com/algorithm/base/bitmap/BitMap.java
+++ src/main/java/com/algorithm/base/bitmap/BitMap.java
@@ -2,6 +2,12 @@ package com.algorithm.base.bitmap;
 
 /**
  * Java中char类型占16bit,也即是2个字节
+ * 将数字A的第k位设置为1：A= A|(1<<(k-1))
+ * 将数字A的第k位设置为0：A = A&~(1<<(k-1))
+ * 检测数字的A的第K位：A&(1<<(k-1))!=0
+ *
+ *比如我们经常要用的是否被2整除，一般都写成  if(n % 2 == 0)
+ * 可以换成 if((n&1) == 0)
  * @Classname BitMap
  * @Description TODO
  * @Date 2020/3/27 9:47
@@ -32,6 +38,14 @@ public class BitMap {
     }
 
 
+    public static void main(String[] args) {
+        BitMap bitMap = new BitMap(12);
+        bitMap.set(10);
+
+        System.out.println(bitMap);
+
+
+    }
 
 
 }
