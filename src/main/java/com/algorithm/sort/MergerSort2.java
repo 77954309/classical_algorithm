be6d28ab6cf3875d36d1ec7e07680bd9acc2b956
李孟 <limeng@knowlegene.com>
2020-08-10 19:27:40 +0800

    归并

--- src/main/java/com/algorithm/sort/MergerSort2.java
+++ src/main/java/com/algorithm/sort/MergerSort2.java
@@ -28,6 +28,7 @@ public class MergerSort2 {
 
         //
         merge(a,p,q,r);
+       // mergeBySentry(a,p,q,r);
     }
 
 
@@ -63,6 +64,42 @@ public class MergerSort2 {
         }
     }
 
+    /**
+     * 合并（哨兵）
+     * @param arr
+     * @param p
+     * @param q
+     * @param r
+     */
+    private void mergeBySentry(int[] arr,int p,int q,int r){
+        int[] leftArr = new int[q - p + 2];
+        int[] rightArr = new int[r - q + 1];
+
+        for (int i = 0; i <= q - p ; i++) {
+            leftArr[i] = arr[p+i];
+        }
+
+        //第一个数组添加哨兵（最大值）
+        leftArr[q - p +1] = Integer.MAX_VALUE;
+
+        for (int i = 0; i < r - q; i++) {
+            rightArr[i] = arr[q+i + 1];
+        }
+
+        rightArr[r - q] = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+        int k = p;
+        while (k <= r){
+            if(leftArr[i] <= rightArr[j]){
+                arr[k++] = leftArr[i++];
+            }else{
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+    }
 
     @Test
     public void init(){
---
+++ src/main/java/com/algorithm/sort/QuickSort2.java
@@ -0,0 +1,54 @@
+package com.algorithm.sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Classname QuickSort2
+ * @Description TODO
+ * @Date 2020/8/8 14:04
+ * @Created by limeng
+ */
+public class QuickSort2 {
+
+    public void quickSort(int[] a,int n){
+        quickSortInternally(a,0,n-1);
+    }
+
+    public void quickSortInternally(int[] a,int p,int r){
+        if(p >= r) return;
+
+        int q = partition(a,p,r); //获取分区点
+        quickSortInternally(a,p,q-1);
+        quickSortInternally(a,q+1,r);
+    }
+
+    public int partition(int[] a,int p,int r){
+        int pivot = a[r];
+        int i = p;
+        for (int j = p; j < r; j++) {
+            if(a[j] < pivot){
+                if(i == j){
+                    ++i;
+                }else{
+                    int tmp = a[i];
+                    a[i++] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+
+        int tmp = a[i];
+        a[i] = a[r];
+        a[r] = tmp;
+
+        return i;
+    }
+
+    @Test
+    public void init(){
+        int[] a = {1,5,8,7,10,11};
+        quickSort(a,a.length);
+        Assert.assertNotNull(a);
+    }
+}
