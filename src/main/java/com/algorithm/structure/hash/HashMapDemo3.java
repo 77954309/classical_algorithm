1af7741a5338337eb0a90a45530eeccb7d8de9ac
77954309 <77954309@qq.com>
2020-10-30 21:16:06 +0800

    跳表

---
+++ src/main/java/com/algorithm/structure/hash/HashMapDemo3.java
@@ -0,0 +1,16 @@
+package com.algorithm.structure.hash;
+
+/**
+ * @Classname HashMapDemo3
+ * @Description TODO
+ * @Date 2020/10/24 15:58
+ * @Created by limeng
+ */
+public class HashMapDemo3 {
+
+//接口约定根据index确定hash
+interface HashFunctionHost{
+
+}
+}
+
