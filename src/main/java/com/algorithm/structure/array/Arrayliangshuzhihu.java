27405ca6c3954d7493c4b26bf18dd97fdeb0f454
77954309 <77954309@qq.com>
2020-11-20 21:14:47 +0800

    力扣

---
+++ src/main/java/com/algorithm/structure/array/Arrayliangshuzhihu.java
@@ -0,0 +1,18 @@
+package com.algorithm.structure.array;
+
+import java.util.HashMap;
+
+/**
+ * @Classname Arrayliangshuzhihu
+ * @Description TODO
+ * @Date 2020/11/20 20:30
+ * @Created by limeng
+ */
+public class Arrayliangshuzhihu {
+    public static void main(String[] args) {
+        int[] nums = {0, 4, 0};
+        int target = 0;
+
+
+    }
+}
