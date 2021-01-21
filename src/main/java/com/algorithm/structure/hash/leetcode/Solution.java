c8553828eec2db3cae4e190f93dff1e81875e888
77954309 <77954309@qq.com>
2021-01-21 22:08:04 +0800

    hash

--- src/main/java/com/algorithm/structure/hash/leetcode/Solution.java
+++ src/main/java/com/algorithm/structure/hash/leetcode/Solution.java
@@ -3,7 +3,7 @@ package com.algorithm.structure.hash.leetcode;
 import com.sun.javafx.collections.MappingChange;
 import org.junit.Test;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * @Classname Solution
@@ -14,7 +14,117 @@ import java.util.HashMap;
 public class Solution {
     @Test
     public void init(){
-        isAnagram("cd","ab");
+       // isAnagram("cd","ab");
+      //  groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+
+        twoSum2(new int[]{3,2,3},6);
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 你可以按任意顺序返回答案。
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     *
+     * 示例 2：
+     *
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     *示例 3：
+     *
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     *  
+     * 提示：
+     *
+     *  2 <= nums.length <= 103
+     *  -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     *出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int l = nums.length;
+        int res = 0;
+        int[] ress = new int[2];
+        for (int i = 0; i < l; i++) {
+            res = nums[i];
+            for (int j = i+1; j < l; j++) {
+                res += nums[j];
+                if(res == target){
+                    ress[0] = i;
+                    ress[1] = j;
+                    break;
+                }
+                res -= nums[j];
+            }
+        }
+
+        return null;
+    }
+    /**
+     * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+     * 示例:
+     *
+     * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * 输出:
+     * [
+     *   ["ate","eat","tea"],
+     *   ["nat","tan"],
+     *   ["bat"]
+     * ]
+     * 说明：
+     *
+     * 所有输入均为小写字母。
+     * 不考虑答案输出的顺序。
+     * @param strs
+     */
+    public void groupAnagrams(String[] strs){
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String s:strs){
+            int l = s.length();
+            int[] counts = new int[26];
+            for (int i = 0; i < l; i++) {
+                counts[s.charAt(i) - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if(counts[i] != 0){
+                    sb.append((char)('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key,list);
+        }
+
+        //new ArrayList<>(map.values());
+        System.out.println(map.values());
+
+
     }
 
     /**
