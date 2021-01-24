5fdc084c34ce47cf72e360a9665627b9c40180ce
77954309 <77954309@qq.com>
2021-01-24 15:55:09 +0800

    Hash

--- src/main/java/com/algorithm/structure/hash/leetcode/Solution.java
+++ src/main/java/com/algorithm/structure/hash/leetcode/Solution.java
@@ -22,9 +22,178 @@ public class Solution {
       //  threeSum(new int[]{-2,0,1,1,2});
 
        // HashMap<String,String> map1 = new HashMap<>();
-        threeSum(new int[]{-1,0,1,2,-1,-4});
+       // threeSum2(new int[]{3,0,-2,-1,1,2});
+
+        fourSum(new int[]{1, 0, -1, 0, -2, 2},0);
     }
 
+    public List<List<Integer>> fourSum2(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return quadruplets;
+        }
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return quadruplets;
+    }
+
+
+    /**
+     * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+     *
+     * 注意：
+     *
+     * 答案中不可以包含重复的四元组。
+     *
+     * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+     *
+     * 满足要求的四元组集合为：
+     * [
+     *   [-1,  0, 0, 1],
+     *   [-2, -1, 1, 2],
+     *   [-2,  0, 0, 2]
+     * ]
+     *
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return list;
+        }
+
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int first = 0; first < n; first++) {
+            if(first > 0 && nums[first] == nums[first-1]){
+                continue;
+            }
+            int firstTarget = target;
+            firstTarget -= nums[first];
+            for (int second = first+1; second < n; second++) {
+
+                if(second >(first+1) && nums[second] == nums[second-1]){
+                    continue;
+                }
+                int secondTarget = firstTarget;
+                secondTarget -= nums[second];
+                int fourth = n-1;
+                for (int third = second+1; third < n; third++) {
+                    if(third > second+1 && nums[third] == nums[third-1]){
+                        continue;
+                    }
+
+                    while (third < fourth && nums[fourth] + nums[third] > secondTarget){
+                        --fourth;
+                    }
+
+                    if(third == fourth){
+                        break;
+                    }
+
+                    if(secondTarget == nums[fourth] + nums[third]){
+                        List<Integer> childs = new ArrayList<>();
+                        childs.add(nums[first]);
+                        childs.add(nums[second]);
+                        childs.add(nums[third]);
+                        childs.add(nums[fourth]);
+                        list.add(childs);
+                    }
+
+                }
+
+            }
+        }
+        return list;
+    }
+
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int first = 0; first < n; first++) {
+            if(first > 0 && nums[first] == nums[first - 1]){
+                continue;
+            }
+
+            int target = 0;
+            target -= nums[first];
+            int third = n - 1;
+            for (int second = first+1; second < n; second++) {
+
+                if(second > (first+1) && nums[second] == nums[second -1]){
+                    continue;
+                }
+
+                while (second < third && nums[second] + nums[third] > target){
+                    --third;
+                }
+
+                if(second == third){
+                    break;
+                }
+
+                if(target == nums[second] + nums[third]){
+                    List<Integer> childs = new ArrayList<>();
+                    childs.add(nums[first]);
+                    childs.add(nums[second]);
+                    childs.add(nums[third]);
+                    list.add(childs);
+                }
+
+            }
+        }
+
+        return list;
+    }
     /**
      * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
      *
@@ -58,7 +227,6 @@ public class Solution {
 
         HashMap<String, List<Integer>> map1 = new HashMap<>();
         for (int i = 0; i < l; i++) {
-            int third = l-1;
             for (int j = i+1; j < l; j++) {
                 if(map2.containsKey(0-(nums[i]+nums[j]))){
                     int k = map2.get(0-(nums[i]+nums[j]));
