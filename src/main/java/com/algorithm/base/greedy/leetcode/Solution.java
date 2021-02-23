713e3dc20c8f5751f854fba8ce81d297909298a4
77954309 <77954309@qq.com>
2021-02-23 22:05:38 +0800

    递归贪心

--- src/main/java/com/algorithm/base/greedy/leetcode/Solution.java
+++ src/main/java/com/algorithm/base/greedy/leetcode/Solution.java
@@ -13,7 +13,9 @@ import org.junit.Test;
 public class Solution {
     @Test
     public void  init(){
+       // maxProfit(new int[]{1,2,3,4,5});
 
+        lemonadeChange(new int[]{5,5,5,10,20});
     }
 
     /**
@@ -44,7 +46,74 @@ public class Solution {
      * @return
      */
     public int maxProfit(int[] prices) {
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
 
-        return 0;
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);
+        }
+        return dp[length - 1][0];
     }
+
+    public int maxProfit2(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; i++) {
+            ans += Math.max(0,prices[i] - prices[i -1 ]);
+        }
+        return ans;
+    }
+
+    /**
+     *在柠檬水摊上，每一杯柠檬水的售价为 5 美元。
+     * 顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+     * 每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+     * 注意，一开始你手头没有任何零钱。
+     *
+     * 如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+     *
+     * 示例 1：
+     *
+     * 输入：[5,5,5,10,20]
+     * 输出：true
+     * 解释：
+     * 前 3 位顾客那里，我们按顺序收取 3 张 5 美元的钞票。
+     * 第 4 位顾客那里，我们收取一张 10 美元的钞票，并返还 5 美元。
+     * 第 5 位顾客那里，我们找还一张 10 美元的钞票和一张 5 美元的钞票。
+     * 由于所有客户都得到了正确的找零，所以我们输出 true。
+     *
+     *
+     * @param bills
+     * @return
+     */
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(int bill:bills){
+            if(bill == 5){
+                five ++;
+            }else if(bill == 10){
+                if(five == 0){
+                    return false;
+                }
+                five--;
+                ten++;
+            }else{
+                if(five > 0 && ten > 0){
+                    five--;
+                    ten--;
+                }else if(five >= 3){
+                    five -= 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
